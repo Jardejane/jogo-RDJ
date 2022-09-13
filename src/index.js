f0d414a74865ef320e69081b885d33b7538984bf
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-13 16:42:42 -0300

    Added Prettier format

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-require("dotenv").config();
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const swagger = require('./swagger/swagger.route')
+const swagger = require('./swagger/swagger.route');
 
-const connectDataBase = require('./database/database.js')
-const routeRegistration = require('./registration/registration.route')
+const connectDataBase = require('./database/database.js');
+const routeRegistration = require('./registration/registration.route');
 
-const routeLogin = require('./login/login.route')
+const routeLogin = require('./login/login.route');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-connectDataBase()
+connectDataBase();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/inicial", routeRegistration)
-app.use('/login', routeLogin)
-app.use("/api-docs", swagger)
+app.use('/inicial', routeRegistration);
+app.use('/login', routeLogin);
+app.use('/api-docs', swagger);
 
-app.listen(PORT,() =>{
-  console.log(`Servidor rodando na porta ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
